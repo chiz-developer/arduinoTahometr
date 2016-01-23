623544869d1c46abd776b386e8fa6c8f62b0ec2b
chiz-developer <chiz.kharkiv@gmail.com>
2016-01-24 00:27:44 +0200

    Добавил функцию вывода дсятеричных чисел

--- tahometr.ino
+++ tahometr.ino
@@ -1,5 +1,5 @@
 //17.12.15 - вывод готового алгоритма в лед-дисплей
-int inPin = 10;
+int inPin = 4;
 int cnt = 0;
 static unsigned long prevTime = 0;
 static unsigned long currTime = 0;
@@ -10,8 +10,8 @@ int dataPin = 11;//данные
 
 
 void digit(int numb, int rang){
-  byte numbs[10] = {
-    B11000000, B11111001, B10100100, B10110000, B10011001, B10010010, B10000010, B11111000, B10000000, B10010000    };
+  byte numbs[11] = {
+    B11000000, B11111001, B10100100, B10110000, B10011001, B10010010, B10000010, B11111000, B10000000, B10010000, B01111111    };
   byte rangs[4] = {
     B00000001, B00000010, B00000100, B00001000    };
   //выключаю индикатор
@@ -28,7 +28,7 @@ void digit(int numb, int rang){
 
 }
 
-void disp(int number){
+void dispInt(int number){
   int one = 0;
   int ten = 0;
   int hnd = 0;
@@ -71,6 +71,38 @@ void disp(int number){
 
 }
 
+void dispF(float number){
+    int a = round(number*100) / 100;
+    int b = round(number*100) % 100;
+    int one = 0;
+    int ten = 0;
+
+    if(a < 10) { //целая часть 0-9
+      digit(a,2);
+      digit(10,2); //точка
+      }
+    else if(a > 9 && a < 100){ //целая часть 10-99
+      ten = (a % 100) / 10;
+      one = a % 10;
+     
+      digit(ten, 1);
+      digit(one, 2);
+      digit(10,2);    
+      }
+
+    if(b < 10) { //дробная часть 0-9
+      digit(0,3);
+      digit(b,4);
+      }
+    else if(b > 9 && b < 100){ //дробная часть 10-99
+      ten = (b % 100) / 10;
+      one = b % 10;
+     
+      digit(ten, 3);
+      digit(one, 4);   
+      }
+  }
+
 void setup(){
   Serial.begin(9600);
 
@@ -101,7 +133,8 @@ void loop(){
  }else{
    cnt = (60000/(currTime-prevTime));
  }
- disp(cnt);
+ //disp(cnt);
+ dispF(90.09);
 }//end loop
 
 
